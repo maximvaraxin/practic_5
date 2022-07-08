1e9d4d47fc00d7ae7a34e520d514ffcdd5f861b3
Максим Вараксин <maks-02@yandex.ru>
2022-07-08 09:26:47 +0300

    add app_8

---
+++ app_7/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ *   Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+ *   [3, 7, 23, 12] -> 19
+ *   [-4, -6, 89, 6] -> 0
+ */
+
+
+
+namespace App_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write($"Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] mass = GetRandomMass( len );
+
+            Console.Write($"Массив: ");
+            PrintMass(mass);
+            
+            Console.WriteLine( $"Cумма элементов, стоящих на нечётных позицияхе = { SumElement( mass ) } "); 
+        }
+
+        // заполняет массив рандомными в диапазоне 1 : 200
+        static int[] GetRandomMass( int len )
+        {   
+            int[] mass = new int[len];
+
+            for (int i = 0; i < mass.Length; i++)
+			{
+                mass[i] = new Random().Next( 100,200 );
+			}
+
+            return mass;
+
+        }
+
+        // возвращает массив в консоль
+        static void PrintMass( int[] mass )
+        {
+            for (int i = 0; i < mass.Length; i++)
+			{
+                Console.Write($"{mass[i]} ");
+			}
+
+            Console.WriteLine();
+        }
+        
+        // определяет сумму элементов, стоящих на нечётных позициях
+        static int SumElement( int[] mass )
+        {   
+            int result = 0;
+
+            for (int i = 0; i < mass.Length; i++)
+			{   
+                if ( (i + 1 ) % 2 == 1)
+                {
+                    result = result + mass[i];
+                }
+			}
+
+            return result;
+        }    
+    }
+}
---
+++ app_8/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+ *   Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+ *   [3 7 22 2 78] -> 76
+ */
+
+
+
+namespace App_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write($"Введите длину массива: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            double[] mass = GetRandomMass( len );
+
+            Console.Write($"Массив: ");
+            PrintMass(mass);
+            
+            Console.WriteLine( $"Cумма максимального и минимального элемента массива = { SumElement( mass ) } "); 
+        }
+
+        // заполняет массив рандомными в диапазоне 1 : 200
+        static double[] GetRandomMass( int len )
+        {   
+            double[] mass = new double[len];
+
+            for (int i = 0; i < mass.Length; i++)
+			{
+                mass[i] = new Random().Next( 100,200 );
+			}
+
+            return mass;
+
+        }
+
+        // возвращает массив в консоль
+        static void PrintMass( double[] mass )
+        {
+            for (int i = 0; i < mass.Length; i++)
+			{
+                Console.Write($"{mass[i]} ");
+			}
+
+            Console.WriteLine();
+        }
+        
+        // определяет сумму максимального и минимального элемента массива
+        static double SumElement( double[] mass )
+        {   
+            double result = 0;
+            double maxElem = mass[0];
+            double minElem = mass[0];    
+
+            for (int i = 0; i < mass.Length; i++)
+			{   
+                if (mass[i] < minElem)
+                {
+                    minElem = mass[i];
+                }
+                else if( mass[i] > maxElem )
+                {
+                    maxElem = mass[i];
+                }
+			}
+            Console.WriteLine( $"Максимальный элемент массива: {maxElem}" );
+            Console.WriteLine( $"Максимальный элемент массива: {minElem}" );
+            result = maxElem + minElem;
+
+            return result;
+        }    
+    }
+}
